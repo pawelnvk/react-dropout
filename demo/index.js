494f5dac23e6be0a202b631701939ba267fe2daa
Paweł Nowak <pawelnvk@users.noreply.github.com>
2018-10-10 22:17:24 +0200

    Redesign demo page (#8)

--- demo/index.js
+++ demo/index.js
@@ -16,6 +16,14 @@ const navItems = [
   { grade: 3, page: 'Contact', path: '/contact' },
 ];
 
+const Hamburger = ({ isActive }) => (
+  <div className={`hamburger ${isActive ? 'is-active' : ''}`}>
+    <span className="hamburger__line hamburger__line--1"></span>
+    <span className="hamburger__line hamburger__line--2"></span>
+    <span className="hamburger__line hamburger__line--3"></span>
+  </div>
+);
+
 const Logo = () => (
   <div className="logo">
     <Link className="logo__link" to="/">Logo</Link>
@@ -35,14 +43,16 @@ const Nav = () => (
             ))}
             <Dropout.Toggle>
               {({ isToggled, toggle }) => (
-                <li className="navigation__item navigation__item--toggle">
-                  <button className="navigation__link" onClick={toggle}>
+                <li className="navigation__item navigation__item--toggle dropdown">
+                  {isToggled && <button className="dropdown__overlay" onClick={toggle} />}
+                  <button className="navigation__link navigation__link--toggle dropdown__toggle" onClick={toggle}>
+                    <Hamburger isActive={isToggled} />
                     {isToggled ? 'Less' : 'More'}
                   </button>
 
                   <Dropout.Rest>
                     {items => (
-                      <ul className="subnav">
+                      <ul className="subnav dropdown__content">
                         {items.map(({ page, path }) => (
                           <li className="subnav__item" key={path}>
                             <Link className="subnav__link" to={path}>{page}</Link>
@@ -79,7 +89,11 @@ const App = () => (
               exact={exact}
               key={path}
               path={path}
-              render={() => <div className="page u-container">{page}</div>}
+              render={() => (
+                <div className={`page page--${page.toLowerCase()}`}>
+                  <div className="page__content">{page}</div>
+                </div>
+              )}
             />
           ))}
         </Switch>
